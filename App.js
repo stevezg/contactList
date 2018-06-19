ea9bc3c5caa051ab2ecf9c3349865de546992f73
Stephen Anderson <anderssj@usc.edu>
2018-06-19 14:20:22 -0700

    working🧐

--- App.js
+++ App.js
@@ -1,96 +1,85 @@
-// import Example from './examples/0-Switch';
-// import Example from './examples/1-Stack';
-// export default Example;
-
-import React from "react";
+import React from 'react'
 import {
   createStackNavigator,
   createSwitchNavigator,
-  createBottomTabNavigator
-} from "react-navigation";
-import Ionicons from "react-native-vector-icons/Ionicons";
+  createBottomTabNavigator,
+} from 'react-navigation'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
-import AddContactScreen from "./screens/AddContactScreen";
-import SettingsScreen from "./screens/SettingsScreen";
-import ContactListScreen from "./screens/ContactListScreen";
-import ContactDetailsScreen from "./screens/ContactDetailsScreen";
-import LoginScreen from "./screens/LoginScreen";
-import fetchUsers from "./api"
+import AddContactScreen from './screens/AddContactScreen'
+import SettingsScreen from './screens/SettingsScreen'
+import ContactListScreen from './screens/ContactListScreen'
+import ContactDetailsScreen from './screens/ContactDetailsScreen'
+import LoginScreen from './screens/LoginScreen'
+import { fetchUsers } from './api'
 
 const MainStack = createStackNavigator(
   {
     ContactList: ContactListScreen,
     ContactDetails: ContactDetailsScreen,
-    AddContact: AddContactScreen
+    AddContact: AddContactScreen,
   },
   {
-    initialRouteName: "ContactList",
+    initialRouteName: 'ContactList',
     navigationOptions: {
-      headerTintColor: "#a41034",
+      headerTintColor: '#a41034',
       headerStyle: {
-        backgroundColor: "#fff"
-      }
-    }
+        backgroundColor: '#fff',
+      },
+    },
   }
-);
+)
 
 MainStack.navigationOptions = {
   tabBarIcon: ({ focused, tintColor }) => (
-    <Ionicons
-      name={`ios-contacts${focused ? "" : "-outline"}`}
-      size={25}
-      color={tintColor}
-    />
-  )
-};
+    <Ionicons name={`ios-contacts${focused ? '' : '-outline'}`} size={25} color={tintColor} />
+  ),
+}
 
 const MainTabs = createBottomTabNavigator(
   {
     Contacts: MainStack,
-    Settings: SettingsScreen
+    Settings: SettingsScreen,
   },
   {
     tabBarOptions: {
-      activeTintColor: "#a41034"
-    }
+      activeTintColor: '#a41034',
+    },
   }
-);
+)
 
 const AppNavigator = createSwitchNavigator({
   Login: LoginScreen,
-  Main: MainTabs
-});
+  Main: MainTabs,
+})
 
 export default class App extends React.Component {
   state = {
-    contacts: null
-  };
-
-  componentDidMount() {
-    // fetchUsers().then(results => this.setState({ contacts: results }))
-    getUsers()
+    contacts: null,
   }
+  componentDidMount() {
+    this.getUsers()
+  }
+
   getUsers = async () => {
     const results = await fetchUsers()
     this.setState({ contacts: results })
-
   }
-
 
   addContact = newContact => {
     this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact]
-    }));
-  };
+      contacts: [...prevState.contacts, newContact],
+    }))
+  }
 
   render() {
     return (
       <AppNavigator
         screenProps={{
           contacts: this.state.contacts,
-          addContact: this.addContact
+          addContact: this.addContact,
         }}
       />
-    );
+    )
   }
 }
